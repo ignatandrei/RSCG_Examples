eff6bc381c9f5d29c340d1a6b52dcb591eb84a69
ignatandrei <ignatandrei@yahoo.com>
2021-02-15 15:54:25 +0200

    first version

---
+++ ApplicationVersion/src/RSCG_Version/RSCG_Version/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RSCG_Version
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strVersion = ThisAssembly.Info.Version;
+            Console.WriteLine(strVersion);
+
+        }
+    }
+}
