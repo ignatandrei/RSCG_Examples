6b0c45a6f2a53c43eda042042ab98e009ce18bcd
ignatandrei <ignatandrei@yahoo.com>
2021-02-16 07:51:06 +0200

    enums

---
+++ Enum/src/RSCG_Enum/RSCG_Enum/Program.cs
@@ -0,0 +1,22 @@
+﻿using AOPMethodsCommon;
+using System;
+
+namespace RSCG_Enum
+{
+    [AutoEnum(template = EnumMethod.GenerateExtensionCode)]
+    public enum MathematicalOperation
+    {
+        None=0,
+        Add=1,
+        Multiplication=2
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fromInt = enumMathematicalOperation.ParseExactMathematicalOperation(1);
+            var fromString = enumMathematicalOperation.ParseExactMathematicalOperation("add");
+            Console.WriteLine(fromInt + "-"+fromString);
+        }
+    }
+}
