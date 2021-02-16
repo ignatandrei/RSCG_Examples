a978c4ae11c0ad9a09a920e55e2c26d5ebcaae90
ignatandrei <ignatandrei@yahoo.com>
2021-02-16 21:08:17 +0200

    adding generator

---
+++ Generator/Generator/JsonData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Generator
+{
+    
+    public class Generator
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("nuget")]
+        public string[] Nuget { get; set; }
+
+        [JsonPropertyName("link")]
+        public string Link { get; set; }
+
+        [JsonPropertyName("author")]
+        public string Author { get; set; }
+    }
+
+    public class Data
+    {
+        [JsonPropertyName("goodFor")]
+        public string GoodFor { get; set; }
+
+        [JsonPropertyName("existingCode")]
+        public string[] ExistingCode { get; set; }
+
+        [JsonPropertyName("usage")]
+        public string[] Usage { get; set; }
+
+        [JsonPropertyName("generatedCode")]
+        public string[] GeneratedCode { get; set; }
+    }
+
+    public class Links
+    {
+        [JsonPropertyName("blog")]
+        public string Blog { get; set; }
+
+        [JsonPropertyName("video")]
+        public string Video { get; set; }
+    }
+
+    public class Description
+    {
+        [JsonPropertyName("generator")]
+        public Generator Generator { get; set; }
+
+        [JsonPropertyName("data")]
+        public Data Data { get; set; }
+
+        [JsonPropertyName("links")]
+        public Links Links { get; set; }
+
+        public string rootFolder;
+    }
+
+
+}
---
+++ Generator/Generator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Generator
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var templatePost = await File.ReadAllTextAsync("post.txt");
+            string rootPath = @"E:\ignatandrei\RSCG_Examples\";
+            string rootFolder = "Enum";
+            var folder =Path.Combine(rootPath, rootFolder); 
+            var text = await File.ReadAllTextAsync(Path.Combine(folder, "description.json"));
+            var desc= JsonSerializer.Deserialize<Description>(text);
+            desc.rootFolder = rootFolder;
+            var templateScriban = Scriban.Template.Parse(templatePost);
+            var output = templateScriban.Render(desc, member => member.Name);
+            await File.WriteAllTextAsync("a.html", output);
+            
+        }
+    }
+}
