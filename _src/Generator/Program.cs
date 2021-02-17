4360142c421a9a64ff69aca582843e3cbb8c929e
ignatandrei <ignatandrei@yahoo.com>
2021-02-17 12:46:54 +0200

    generating for all

--- _src/Generator/Program.cs
+++ _src/Generator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 namespace Generator
@@ -8,19 +9,37 @@ namespace Generator
     {
         static async Task Main(string[] args)
         {
-            
-            
+            var generators = new string[]
+            {
+                "ApplicationVersion",
+                "Enum",
+                "JsonToClass",
+
+            };
+            var gen =await Task.WhenAll( generators.Select(async it => await Generate(it)).ToArray());
+
+            var posts = gen.Select(async it => await GeneratePost(it)).ToArray();
+
+            await Task.WhenAll(posts);
+        }
+        private static async Task GeneratePost(Description desc)
+        {
             var templatePost = await File.ReadAllTextAsync("post.txt");
-            string rootPath = @"E:\ignatandrei\RSCG_Examples\";
-            string rootFolder = "JsonToClass";// "Enum";
-            var folder =Path.Combine(rootPath, rootFolder); 
-            var text = await File.ReadAllTextAsync(Path.Combine(folder, "description.json"));
-            var desc= JsonSerializer.Deserialize<Description>(text);
-            desc.rootFolder = rootFolder;
             var templateScriban = Scriban.Template.Parse(templatePost);
             var output = templateScriban.Render(desc, member => member.Name);
-            await File.WriteAllTextAsync("a.html", output);
+            await File.WriteAllTextAsync($"{desc.rootFolder}.html", output);
+
+        }
+        private static async Task<Description> Generate(string rootFolder)
+        {
             
+            string rootPath = @"E:\ignatandrei\RSCG_Examples\";
+
+            var folder = Path.Combine(rootPath, rootFolder);
+            var text = await File.ReadAllTextAsync(Path.Combine(folder, "description.json"));
+            var desc = JsonSerializer.Deserialize<Description>(text);
+            desc.rootFolder = rootFolder;
+            return desc;
         }
     }
 }
