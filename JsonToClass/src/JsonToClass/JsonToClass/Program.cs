238daa0bf84d8f966e32524a264d2dda049eb478
ignatandrei <ignatandrei@yahoo.com>
2021-02-17 08:05:39 +0200

    json to class

---
+++ JsonToClass/src/JsonToClass/JsonToClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JsonToClass.Json.Person;
+using Microsoft.Extensions.Configuration;
+
+namespace JsonToClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Person();
+            p1.Blog = "http://msprogrammer.serviciipeweb.ro/";
+            var config = new ConfigurationBuilder()
+              .AddJsonFile("person.json")
+              .Build();
+
+            var p = config.Get<Person>();
+            Console.WriteLine($"{p.FirstName}");
+        }
+    }
+}
