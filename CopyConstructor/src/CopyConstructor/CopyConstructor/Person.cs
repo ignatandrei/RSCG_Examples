fbf60173d5f14348c38e89983486c4fa275414de
ignatandrei <ignatandrei@yahoo.com>
2021-02-18 08:14:50 +0200

    copy constructor starting

---
+++ CopyConstructor/src/CopyConstructor/CopyConstructor/Person.cs
@@ -0,0 +1,15 @@
+﻿using AOPMethodsCommon;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopyConstructor
+{
+
+    [AutoMethods(template = TemplateMethod.CustomTemplateFile, CustomTemplateFileName = "CopyConstructorDestructor.txt")]
+    partial class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ CopyConstructor/src/CopyConstructor/CopyConstructor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CopyConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pOldPerson = new Person();
+            pOldPerson.FirstName = "Andrei";
+            pOldPerson.LastName = "Ignat";
+            var newPerson = new Person(pOldPerson);
+            Console.WriteLine(newPerson.FirstName);
+            var (_, last) = newPerson;
+            Console.WriteLine(last);
+        }
+    }
+}
