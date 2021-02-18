b8ac55d2d844570f80ede67492d40f3639cd2bb0
ignatandrei <ignatandrei@yahoo.com>
2021-02-18 10:24:58 +0200

    stareting map

---
+++ DTOMapper/DTOMapper/DTOMapper/Program.cs
@@ -0,0 +1,43 @@
+﻿using GeneratedMapper.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace DTOMapper
+{
+
+    public class Department
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        
+        public List<string> Employees { get; set; }
+    }
+
+    [IgnoreInTarget("Employees")]
+    [MapFrom(typeof(Department))]
+    public class DepartmentDTO
+    {
+        public int ID { get; set; }
+        public string Name{get; set;}
+
+        [MapWith(typeof(Resolver))]
+        public int EmployeesNr { get; set; }
+
+    }
+    public class Resolver
+    {
+        public int Resolve(object input)
+        {
+            return 10;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
