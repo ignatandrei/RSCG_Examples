494b438549a71fe94d4423fd2b2935274ee83e74
ignatandrei <ignatandrei@yahoo.com>
2021-02-18 12:45:52 +0200

    skinny

---
+++ SkinnyControllers/src/AOPSkinnyController/AOPSkinnyController/Classes/IPersonRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AOPSkinnyController.Classes
+{
+    public class Person {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    public class PersonRepository
+    {
+        public Person Get(int id)
+        {
+            return new Person()
+            {
+
+            }
+        }
+    }
+    public class IPersonRepository
+    {
+    }
+}
