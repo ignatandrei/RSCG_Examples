1dcfa868bbc9dde03cbc28078841e8ed11411c2e
ignatandrei <ignatandrei@yahoo.com>
2021-03-01 07:17:37 +0200

    formattable

---
+++ IFormatter/src/RSCG_IFormattter/RSCG_IFormattter/Program.cs
@@ -0,0 +1,40 @@
+﻿using AOPMethodsCommon;
+using System;
+
+namespace RSCG_IFormattter
+{
+    [AutoMethods(CustomTemplateFileName = "CreateFormattable.txt", template = TemplateMethod.CustomTemplateFile)]
+    partial class Department
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+    [AutoMethods(CustomTemplateFileName = "CreateFormattable.txt", template = TemplateMethod.CustomTemplateFile)]
+    partial class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public Department dep { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var e = new Employee();
+            e.ID = 1;
+            e.Name = "Andrei";
+            e.dep = new Department();
+            e.dep.Name = "IT";
+
+            Console.WriteLine(e.ToString("for employee with id = {id} the name is {name} and department is {dep?.Name}", null));
+
+            e.dep = null;
+
+            Console.WriteLine(e.ToString("for employee with id = {id} the name is {name} and department is {dep?.Name}", null));
+
+        }
+    }
+}
