ce3ebcd48ad54be0f167d6e6945234913e3771bb
ignatandrei <ignatandrei@yahoo.com>
2021-03-01 08:44:51 +0200

    decorator

--- DP_Decorator/src/DP_Decorator/DP_Decorator/Program.cs
+++ DP_Decorator/src/DP_Decorator/DP_Decorator/Program.cs
@@ -2,14 +2,12 @@
 
 namespace DP_Decorator
 {
-    class Program
-    {
         public interface ICoffee
         {
             public int Price { get; set; }
             public string Description { get; set; }
         }
-
+        
         public class SimpleCoffee : ICoffee
         {
             public SimpleCoffee()
@@ -32,8 +30,9 @@ namespace DP_Decorator
         }
 
 
-        public class MilkDecorator: ICoffee
+        public partial class MilkDecorator: ICoffee
         {
+            [BeaKona.AutoInterface(TemplateLanguage = "scriban", TemplateBody = TemplateBody)]
             private readonly ICoffee coffee;
 
             public MilkDecorator(ICoffee coffee)
@@ -41,22 +40,112 @@ namespace DP_Decorator
                 this.coffee = coffee;
             }
 
+            const string TemplateBody = @"
+{{~for method in methods~}}
+{{method.is_async?""async "":""""}}{{method.return_type}} {{interface}}.{{method.name}}({{method.arguments_definition}})
+{
+{{~if method.is_async~}}
+    {{~for reference in references~}}
+    var temp{{for.index}} = (({{interface}})this.{{reference}}).{{method.name}}({{method.call_arguments}}).ConfigureAwait(false);
+    {{~end~}}
+    {{~for reference in references~}}
+    {{for.last && method.return_expected ? ""return "" : """"}}await temp{{for.index}};
+    {{~end~}}
+{{~else~}}
+    {{~for reference in references~}}
+    {{for.last && method.return_expected ? ""return "" : """"}}(({{interface}})this.{{reference}}).{{method.name}}({{method.call_arguments}});
+    {{~end~}}
+{{~end~}}
+}
+
+{{~end~}}
+{{~for property in properties~}}
+{{property.type}} {{interface}}.{{property.name}}
+{
+{{~if property.have_getter~}}
+   get
+   {
+        {{ if property.name == 'Description'}}
+            var name = this.GetType().Name;
+            return (({{interface}})this.{{references[0]}}).{{property.name}} + "" with "" + name;
+        {{else}}
+          return (({{interface}})this.{{references[0]}}).{{property.name}};
+        {{end}}
+   }
+{{~end~}}
+{{~if property.have_setter~}}
+   set
+   {
+      {{~for reference in references~}}
+      (({{interface}})this.{{reference}}).{{property.name}} = value;
+      {{~end~}}
+   }
+{{~end~}}
+}
+
+{{~end~}}
+{{~for indexer in indexers~}}
+{{indexer.type}} {{interface}}.{{indexer.name}}[{{indexer.parameters_definition}}]
+{
+{{~if indexer.have_getter~}}
+   get
+   {
+      return (({{interface}})this.{{references[0]}})[{{indexer.call_parameters}}];
+   }
+{{~end~}}
+{{~if indexer.have_setter~}}
+   set
+   {
+      {{~for reference in references~}}
+      (({{interface}})this.{{reference}})[{{indexer.call_parameters}}] = value;
+      {{~end~}}
+   }
+{{~end~}}
+}
+
+{{~end~}}
+{{~for event in events~}}
+event {{event.type}} {{interface}}.{{event.name}}
+{
+   add
+   {
+      {{~for reference in references~}}
+      (({{interface}})this.{{reference}}).{{event.name}} += value;
+      {{~end~}}
+   }
+   remove
+   {
+      {{~for reference in references~}}
+      (({{interface}})this.{{reference}}).{{event.name}} -= value;
+      {{~end~}}
+   }
+}
+
+{{~end~}}
+";
 
         }
-        public class MilkDecorator : ICoffee
-        {
-            private readonly ICoffee coffee;
+    //public class MilkDecorator : ICoffee
+    //{
+    //    private readonly ICoffee coffee;
 
-            public MilkDecorator(ICoffee coffee)
-            {
-                this.coffee = coffee;
-            }
+    //    public MilkDecorator(ICoffee coffee)
+    //    {
+    //        this.coffee = coffee;
+    //    }
 
 
-        }
+    //}
+    class Program
+    {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SimpleCoffee s = new SimpleCoffee();
+            Console.WriteLine(s.Description);
+            ICoffee m = new MilkDecorator(s);
+            Console.WriteLine(m.Description);
         }
+        
     }
 }
