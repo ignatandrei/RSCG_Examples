5ae299319dac1b84b3b6a30c3009e422b571db4c
ignatandrei <ignatandrei@yahoo.com>
2021-03-02 11:58:49 +0200

    with database

---
+++ PropertyExpressionGenerator/src/PropGen/PropGen/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PropGen
+{
+    public partial class DatabaseContext : DbContext
+    {
+        public virtual DbSet<Person> Person { get; set; }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Person>()
+                .HasKey(iterator => iterator.ID)
+                
+                ;
+        }
+    }
+}
--- PropertyExpressionGenerator/src/PropGen/PropGen/Program.cs
+++ PropertyExpressionGenerator/src/PropGen/PropGen/Program.cs
@@ -1,8 +1,11 @@
 ﻿using AOPMethodsCommon;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace PropGen
 {
@@ -17,7 +20,7 @@ namespace PropGen
         public DateTime? DateOfBirth {get;set;}
 
     }
-    public class PersonList : List<Person>
+        public class PersonList : List<Person>
     {
         public Expression<Func<Person,bool>> FindEx(string nameProp, SearchCriteria s, object value)
         {
@@ -26,11 +29,36 @@ namespace PropGen
     }
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var sc = new ServiceCollection();
+            sc.AddDbContext<DatabaseContext>(opt =>
+            {
+                opt.UseInMemoryDatabase("myMemory");
+            });
+
+            var diContainer = sc.BuildServiceProvider();
+            var cnt = diContainer.GetService<DatabaseContext>();
+            for (int i = 1; i < 100 ; i++)
+            {
+                var p = new Person();
+                p.ID = 0;
+                p.FirstName = "Andrei "+i;
+                p.LastName = (i % 2 == 0) ? "" : "Ignat" + i;
+                p.DateOfBirth = (i % 2 == 0) ? null : new DateTime(1970, 4, 16);
+                cnt.Person.Add(p);
+            }
+            await cnt.SaveChangesAsync();
             var s = Metadata_Person.expr_FirstName_Contains("asd");
-            var p = new PersonList();
-            var q = p.Where(s.Compile());
+
+            var queryCnt = Metadata_Person.expr_FirstName_Contains("9");
+            var p9 = await cnt.Person.Where(queryCnt).ToArrayAsync();
+            Console.WriteLine(p9.Length);
+            var queryID = Metadata_Person.expr_ID_equal(7);
+            var pId = await cnt.Person.FirstOrDefaultAsync(queryID);
+            Console.WriteLine(pId.FirstName);
+            //var p = new PersonList();
+            //var q = p.Where(s.Compile());
         }
     }
 }
