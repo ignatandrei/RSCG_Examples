cee6d8b4aadfb428bb6f94fd091131d94b1e7db2
ignatandrei <ignatandrei@yahoo.com>
2021-03-03 08:12:29 +0200

    trying transplator

---
+++ TemplateRender/src/Rendering/Rendering/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Rendering
+{
+    public partial class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Department Department { get; set; }
+    }
+    public class Department
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var IT = new Department();
+            IT.Name = "IT";
+            var e = new Employee();
+            e.ID = 10;
+            e.Name = "Andrei Ignat";
+            e.Department = IT;
+
+
+        }
+    }
+}
