291b49f80bc880ed6dc80ebb2e4fcc05d7e906bb
ignatandrei <ignatandrei@yahoo.com>
2021-03-07 22:58:27 +0200

    more formatting

--- print/getBook.bat
+++ print/getBook.bat
@@ -5,4 +5,5 @@ docker container kill print_rscg
 docker container rm print_rscg
 
 rem pandoc.exe -f markdown -s -o index.docx ../book/about.md ../book/whatIs.md ../ApplicationVersion/README.md --metadata title="RSCG examples" --toc
-pandoc.exe  -d pandocHTML.yaml > ../docs/index.html
+# pandoc.exe  -d pandocHTML.yaml > ../docs/index.html
+pandoc.exe -d .\pandocHTML.yaml
