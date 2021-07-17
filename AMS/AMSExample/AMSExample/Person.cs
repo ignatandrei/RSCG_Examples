3b761c4dea02de6a46f09fa6fb17ff45b1165e8b
ignatandrei <ignatandrei@yahoo.com>
2021-07-17 06:43:15 +0300

    start add ams

---
+++ AMS/AMSExample/AMSExample/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AMSExample
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string  Name { get; set; }
+    }
+}
---
+++ AMS/AMSExample/TestAMSWebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using AMSExample;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAMSWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+      
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Person Get()
+        {
+            return new Person()
+            {
+                Name = "Ignat Andrei"
+
+            };
+        }
+    }
+}
