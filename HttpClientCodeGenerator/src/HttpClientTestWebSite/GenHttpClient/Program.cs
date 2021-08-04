be1aa20734a887fcf401e95cf02c478670b1cba3
ignatandrei <ignatandrei@yahoo.com>
2021-08-05 02:35:48 +0300

    try to add example

---
+++ HttpClientCodeGenerator/src/HttpClientTestWebSite/GenHttpClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HttpClientGenerator.Shared;
+
+namespace GenHttpClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5000");
+                var userService = new WeatherService(client);
+                var w = await userService.GetAllWeather();
+                Console.WriteLine($"{w.First().Summary}");
+            }
+        }
+    }
+
+    public partial class WeatherService
+    {
+       [HttpGet("WeatherForecast/{id}")]
+       public partial Task<WeatherForecast> GetWeather(int id);
+
+        [HttpPost("WeatherForecast")]
+        public partial Task<WeatherForecast[]> GetAllWeather();
+    }
+}
---
+++ HttpClientCodeGenerator/src/HttpClientTestWebSite/HttpClientTestWebSite/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+﻿using GenHttpClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpClientTestWebSite.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        
+        [HttpGet("{id}")]
+        public WeatherForecast Get(int id)
+        {
+
+            return Get().First();
+        }
+    }
+}
