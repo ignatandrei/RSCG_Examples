9860059d74ae94e5870e742082e08500f90512c7
ignatandrei <ignatandrei@yahoo.com>
2021-08-21 12:21:41 +0300

    start tiny types

---
+++ TinyTypes/src/TinyTypes/TinyBLLogic/Class1.cs
@@ -0,0 +1,35 @@
+﻿using AD.BaseTypes;
+using System;
+
+namespace TinyBLLogic
+{
+    [Int] public partial record EmployeeId;
+    [Int] public partial record DepartmentId;
+
+
+    public class Employee
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int? DepartmentId { get; set; }
+    }
+    public class EmployeeRepository
+    {
+        public Employee GetFromId(DepartmentId departmentId,  EmployeeId employeeId)
+        {
+            return GetFromId(departmentId, employeeId);
+        }    
+        public Employee GetFromId(int idDepartment, int idEmployee)
+        {
+            
+            return new Employee()
+            {
+                ID = idEmployee,
+                DepartmentId = idDepartment,
+                Name = "Andrei " + idEmployee
+
+            };
+        }
+    }
+}
---
+++ TinyTypes/src/TinyTypes/TinyConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TinyBLLogic;
+
+namespace TinyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var e = new EmployeeRepository();
+            e.GetFromId(10, 34);
+            e.GetFromId(new DepartmentId(34), new EmployeeId(10));
+        }
+    }
+}
