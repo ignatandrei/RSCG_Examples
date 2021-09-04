101b046d11224c675276e4d2e5221271979bbcc9
ignatandrei <ignatandrei@yahoo.com>
2021-09-04 10:57:46 +0300

    save

--- book/other.ps1
+++ book/other.ps1
@@ -1,15 +1,26 @@
+# Install-Module FormatMarkdownTable
 $others=  Get-Content -Raw -Path other.txt | ConvertFrom-Json
 $data = $others.data
 #Write-Host $data
-$i = 1;
+[System.Collections.ArrayList]$allData = @()
+
 $data | %{
 	$repo= $_.repository
  Write-Host "processing" $repo
- $api = $repo.Replace("https://github.com","https://api.github.com")
+ $api = $repo.Replace("https://github.com","https://api.github.com/repos")
  Write-Host "processing" $api
  
- $apiResponse = (curl  $repo)
- Write-Host "processing" $apiResponse.Content.FullName
+ $apiResponse = (curl  $api) | ConvertFrom-Json
+ #Write-Host "processing" $apiResponse
+ Write-Host "processing" $apiResponse.full_name	
+ $allData.Add($apiResponse)
+	Write-Host "nr " $allData.Count
+
  break;
  	
 }
+
+Write-Host "!1nr " 
+
+#$mark = $allData | Format-MarkdownTableListStyle full_name, url
+#Write-Host $mark
