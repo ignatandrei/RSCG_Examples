5fb2667c9d7e95a7d0a12c6d13d4736963f979bb
ignatandrei <ignatandrei@yahoo.com>
2021-09-04 11:28:14 +0300

    now it works

--- book/other.ps1
+++ book/other.ps1
@@ -1,11 +1,13 @@
 # Install-Module FormatMarkdownTable
-$others=  Get-Content -Raw -Path other.txt1 | ConvertFrom-Json
+$others=  Get-Content -Raw -Path other.txt | ConvertFrom-Json
 $data = $others.data
 #Write-Host $data
 [System.Collections.ArrayList]$allData = @()
-
+$i=0
 $data | %{
-	$repo= $_.repository
+ 
+ $i=$i+1
+ $repo= $_.repository
  Write-Host "processing" $repo
  $api = $repo.Replace("https://github.com","https://api.github.com/repos")
  Write-Host "processing" $api
@@ -13,6 +15,8 @@ $data | %{
  $apiResponse = (curl  $api) | ConvertFrom-Json
  #Write-Host "processing" $apiResponse
  Write-Host "processing" $apiResponse.full_name	
+ $apiResponse| add-member -Name "Nr" -value $i -MemberType NoteProperty
+ 
  $allData.Add($apiResponse)
 	
  
@@ -20,6 +24,6 @@ $data | %{
 }
 Write-Host "nr " $allData.Count
 #| Select full_name,html_url
-$allData  | Format-MarkdownTableTableStyle full_name,html_url,description -ShowMarkdown  
+$allData  | Format-MarkdownTableTableStyle Nr,full_name,html_url,description -ShowMarkdown  
 
 Write-Host 
