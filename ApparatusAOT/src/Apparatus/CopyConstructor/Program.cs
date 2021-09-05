dc7854cf322cd9e177eba5cfc257a8fcf122b4db
ignatandrei <ignatandrei@yahoo.com>
2021-09-05 20:19:25 +0300

    apparatus

---
+++ ApparatusAOT/src/Apparatus/CopyConstructor/Program.cs
@@ -0,0 +1,18 @@
+﻿using Apparatus.AOT.Reflection;
+using System;
+
+namespace CopyConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pOldPerson = new Person();
+            var prop = pOldPerson.GetProperties().Values ;
+            foreach (var item in prop)
+            {
+                Console.WriteLine($"{item.Name} Attr: {item.Attributes.Length}");
+            }            
+        }
+    }
+}
