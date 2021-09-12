79709f8c87e4fd541e17153f7b2e69d8d320ad5e
ignatandrei <ignatandrei@yahoo.com>
2021-09-12 22:04:05 +0300

    added apparatus

--- ApparatusAOT/src/Apparatus/CopyConstructor/Program.cs
+++ ApparatusAOT/src/Apparatus/CopyConstructor/Program.cs
@@ -8,10 +8,16 @@ namespace CopyConstructor
         static void Main(string[] args)
         {
             var pOldPerson = new Person();
+            pOldPerson.FirstName = "Andrei";
+
             var prop = pOldPerson.GetProperties().Values ;
             foreach (var item in prop)
             {
-                Console.WriteLine($"{item.Name} Attr: {item.Attributes.Length}");
+                Console.WriteLine($"{item.Name} Attr: {item.Attributes.Length} value {item.Name}");
+                if(item.TryGetValue(pOldPerson, out var val)){
+                    Console.WriteLine("value : " + val);
+                }
+                
             }            
         }
     }
