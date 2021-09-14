f68329c4a830adf8b103b18325c3490134015d83
ignatandrei <ignatandrei@yahoo.com>
2021-09-14 23:11:16 +0300

    time bomb

---
+++ TimeBombComment/src/TimeBombComment/Console_TimeBombComment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Console_TimeBombComment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TB: 2021-09-13 this is a comment transformed into an error
+            //TB: and this is a warning
+            //TB: 2022-12-30 and this should not appear yet
+            Console.WriteLine("See the TB comment above ? ");
+        }
+    }
+}
