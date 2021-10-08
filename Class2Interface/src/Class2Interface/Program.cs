b68529ab1bebf154ec7d615e0380d5345dbb5dfb
ignatandrei <ignatandrei@yahoo.com>
2021-10-08 10:49:32 +0300

    CI

---
+++ Class2Interface/src/Class2Interface/Program.cs
@@ -0,0 +1,32 @@
+﻿using BoilerplateFree;
+using System;
+using System.Diagnostics;
+
+namespace Class2Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPerson p = new Person();
+            p.Name = "Andrei";
+            p.Foo();
+            Console.WriteLine(p.Name);
+        }
+    }
+
+    [AutoGenerateInterface]
+    public partial class Person: IPerson
+    {
+        public void Foo()
+        {
+            Console.WriteLine("Foo");
+        }
+        //dummy
+        private string s { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        //dummy
+        public static string NewID { get; set; }
+    }
+}
