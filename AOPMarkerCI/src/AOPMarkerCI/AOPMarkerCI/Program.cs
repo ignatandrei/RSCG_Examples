d6286687b3aca6d4c863a06f9ec31a8fefa0a516
ignatandrei <ignatandrei@yahoo.com>
2021-10-15 20:50:26 +0300

    start aopmarkert

---
+++ AOPMarkerCI/src/AOPMarkerCI/AOPMarkerCI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AOPMarkerCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int i = Method1();
+            Console.WriteLine($"result:{i}");
+        }
+        static int Method1()
+        {
+            var ret = Method2(DateTime.Now);
+            return ret % 2 == 0 ? 1 : 0;
+        }
+
+        static int Method2(DateTime now)
+        {
+            return now.Second;
+        }
+    }
+}
