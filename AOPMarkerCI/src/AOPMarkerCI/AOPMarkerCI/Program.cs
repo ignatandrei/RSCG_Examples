57e6cfb5c26329676370256b7d5428f0f39638de
ignatandrei <ignatandrei@yahoo.com>
2021-10-16 06:23:30 +0300

    first try

--- AOPMarkerCI/src/AOPMarkerCI/AOPMarkerCI/Program.cs
+++ AOPMarkerCI/src/AOPMarkerCI/AOPMarkerCI/Program.cs
@@ -1,21 +1,25 @@
-﻿using System;
+﻿using AOPMethodsCommon;
+using System;
 
 namespace AOPMarkerCI
 {
+    [AutoMethods(template = TemplateMethod.CustomTemplateFile, MethodPrefix = "auto", CustomTemplateFileName = "../AutoMethod.txt")]
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Run the autoci file");
             int i = Method1();
             Console.WriteLine($"result:{i}");
         }
+        [AOPMarkerMethod]
         static int Method1()
         {
             var ret = Method2(DateTime.Now);
             return ret % 2 == 0 ? 1 : 0;
         }
-
+        [AOPMarkerMethod]
         static int Method2(DateTime now)
         {
             return now.Second;
