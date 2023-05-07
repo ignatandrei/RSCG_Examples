c76b71c485c469dceb1a2662bbc64e9ad81ff967
Andrei Ignat <ignatandrei@yahoo.com>
2023-05-07 22:18:46 +0300

    Update docusaurus.config.js

--- v2/rscg_examples_site/docusaurus.config.js
+++ v2/rscg_examples_site/docusaurus.config.js
@@ -11,16 +11,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://ignatandrei.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/RSCG_Examples/v2/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'ignatandrei', // Usually your GitHub org/user name.
+  projectName: 'RSCG_Examples', // Usually your repo name.
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
