9545ab3ad252cedbda2cf424fcd0253dc5643d25
Andrei Ignat <ignatandrei@yahoo.com>
2023-07-12 02:23:13 +0300

    adding example

--- v2/rscg_examples/mapperly/src/mapperlyDemo/Program.cs
+++ v2/rscg_examples/mapperly/src/mapperlyDemo/Program.cs
@@ -1,7 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using mapperlyDemo;
-
-Console.WriteLine("Hello, World!");
 var p=new Person();
 p.FirstName = "Andrei";
 p.LastName = "Ignat";
