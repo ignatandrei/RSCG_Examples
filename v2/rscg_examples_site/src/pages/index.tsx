8ea4f5dfa48bcb164317adbd7eef23eba1c67588
Andrei Ignat <ignatandrei@yahoo.com>
2023-08-05 10:48:05 +0300

    added missing files

--- v2/rscg_examples_site/src/pages/index.tsx
+++ v2/rscg_examples_site/src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/category/rscg-examples">
+            to="/docs/List-of-RSCG">
             See examples 
           </Link>
         </div>
