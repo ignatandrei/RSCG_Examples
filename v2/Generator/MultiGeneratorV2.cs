5c57eb8062f5905b03d7f96ce3578619e9bd5bfd
Andrei Ignat <ignatandrei@yahoo.com>
2023-10-19 12:36:20 +0300

    added

--- v2/Generator/MultiGeneratorV2.cs
+++ v2/Generator/MultiGeneratorV2.cs
@@ -161,6 +161,7 @@ new("AutoEmbed https://github.com/chsienki/AutoEmbed                           "
 ,new NoExample("PolymorphicJsonSourceGenerator https://github.com/harrhp/PolymorphicJsonSourceGenerator/","work just with records")
 , new NoExample("VisitorPatternGenerator https://github.com/hikarin522/VisitorPatternGenerator/",WaitingForIssue)
 , new NoExample("DynamicsMapper https://github.com/YonatanCohavi/DynamicsMapper",WaitingForIssue)
+, new NoExample("Pipelines https://github.com/DumplingsDevs/Pipelines/",old)
     };
     
      //there are more https://ignatandrei.github.io/RSCG_Examples/v2/docs/CommunityToolkit.Mvvm
