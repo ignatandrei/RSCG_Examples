3205cf75dbf3321e7731e88b181c916fd8a4abe7
Andrei Ignat <ignatandrei@yahoo.com>
2023-11-04 17:07:50 +0200

    added package

--- v2/rscg_examples/Biwen.AutoClassGen/src/FromInterface/IPerson.cs
+++ v2/rscg_examples/Biwen.AutoClassGen/src/FromInterface/IPerson.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using Biwen.AutoClassGen.Attributes;
 
 namespace FromInterface;
 
--- v2/rscg_examples/Biwen.AutoClassGen/src/FromInterface/Person.cs
+++ v2/rscg_examples/Biwen.AutoClassGen/src/FromInterface/Person.cs
@@ -1,8 +1,7 @@
 ﻿namespace FromInterface;
 public partial class Person : IPerson
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+   
 
     public string FullName() { return FirstName + " " + LastName; }
 
