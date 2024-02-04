870a9deed1acf7d57b520b4666407f9d1cff1768
Andrei Ignat <ignatandrei@yahoo.com>
2024-02-04 21:05:56 +0200

    Update lastGenerator variable to use "NetAutomaticInterface"

--- v2/Generator/MultiGeneratorV2.cs
+++ v2/Generator/MultiGeneratorV2.cs
@@ -905,7 +905,7 @@ new("AutoEmbed https://github.com/chsienki/AutoEmbed                           "
         var x = 0;
         x++;
         //if(x>2)
-        var lastGenerator = "WIAD";
+        var lastGenerator = "NetAutomaticInterface";
         var latest = generators[lastGenerator];
         await Task.WhenAll(_AllDescriptions
             .OrderByDescending(it => it.generatedDate)
