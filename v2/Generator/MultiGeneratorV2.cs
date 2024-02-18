e9a8c8e608cf5cabe0a12c859e3a1b6efe7f88fd
Andrei Ignat <ignatandrei@yahoo.com>
2024-02-18 17:59:09 +0200

    Add new image file and update configuration

--- v2/Generator/MultiGeneratorV2.cs
+++ v2/Generator/MultiGeneratorV2.cs
@@ -393,6 +393,7 @@ new("AutoEmbed https://github.com/chsienki/AutoEmbed                           "
             text = text.Replace("(uml/GenericsTypeDeclaration.png)", $"({d.Generator!.Source}/uml/GenericsTypeDeclaration.png)");
             text = text.Replace("(uml/InheritanceRelationsips.png)", $"({d.Generator!.Source}/uml/InheritanceRelationsips.png)");
             text = text.Replace("(uml/Associations.png)", $"({d.Generator!.Source}/uml/Associations.png)");
+            text = text.Replace("(uml/CustomAssociation.png)", $"({d.Generator!.Source}/uml/CustomAssociation.png)");
             text = text.Replace("(uml/RecordParameterList.png)", $"({d.Generator!.Source}/uml/RecordParameterList.png)");
             text = text.Replace("(UnionsGenerator)", $"({d.Generator!.Source}/UnionsGenerator");
             text = text.Replace("(UtilityGenerators)", $"({d.Generator!.Source}/UtilityGenerators");
