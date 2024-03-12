05046269081ae5dd8d7d1550bc0bfd4adca58cf3
Andrei Ignat <ignatandrei@yahoo.com>
2024-03-12 21:34:50 +0200

    Update MultiGeneratorV2.cs

--- v2/Generator/MultiGeneratorV2.cs
+++ v2/Generator/MultiGeneratorV2.cs
@@ -158,7 +158,7 @@ new("AutoEmbed https://github.com/chsienki/AutoEmbed                           "
 ,new("Mediator https://github.com/martinothamar/Mediator",later)
 ,new("NSourceGenerators https://github.com/NeVeSpl/NSourceGenerators/",old)
 ,new ("ReForge.Union https://github.com/nalcorso/ReForge.Union",inspirational)
-//,new("",later)
+,new("AutoConstructor https://github.com/k94ll13nn3/AutoConstructor",later)
 //,new("",later)
 //,new("",later)
 };
