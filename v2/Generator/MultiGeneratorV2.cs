206d1d648bf49bf0fbcfc79e44862e8d0fbcde3d
Andrei Ignat <ignatandrei@yahoo.com>
2024-05-09 20:22:13 +0300

    mixin

--- v2/Generator/MultiGeneratorV2.cs
+++ v2/Generator/MultiGeneratorV2.cs
@@ -1010,7 +1010,7 @@ new("AutoEmbed https://github.com/chsienki/AutoEmbed                           "
         var x = 0;
         x++;
         //if(x>2)
-        var lastGenerator = "MinimalApis.Discovery";
+        var lastGenerator = "Minerals.AutoMixins";
         var latest = generators[lastGenerator];
         await Task.WhenAll(_AllDescriptions
             .OrderByDescending(it => it.generatedDate)
