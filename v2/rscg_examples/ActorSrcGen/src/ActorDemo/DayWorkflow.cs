80c672f86ab398961c980938fc2e69cf210e667a
Andrei Ignat <ignatandrei@yahoo.com>
2024-05-11 17:42:48 +0300

    first version

---
+++ v2/rscg_examples/ActorSrcGen/src/ActorDemo/DayWorkflow.cs
@@ -0,0 +1,55 @@
+﻿using ActorSrcGen;
+using System.Diagnostics.Metrics;
+
+namespace ActorDemo;
+[Actor]
+partial class DayWorkflow
+{
+    [FirstStep("StartDay")]
+    //[Receiver]
+    [NextStep(nameof(WashFace))]
+    [NextStep(nameof(LogMessage))]
+    public async Task<Person> StartDay(Person p)
+    {
+        await Task.Delay(1000 );
+        return p;
+    }
+
+    
+
+    [Step]
+    [NextStep(nameof(LogMessage))]
+    [NextStep(nameof(Eat))]
+    public async Task<Person> WashFace(Person p)
+    {
+        await Task.Delay(1000);
+        return p;
+    }
+    
+
+
+    [Step]
+    [NextStep(nameof(LogMessage))]
+    [NextStep(nameof(Sleep))]
+    public async Task<Person> Eat(Person p)
+    {
+        await Task.Delay(1000);
+        return p;
+    }
+    
+
+    [NextStep(nameof(LogMessage))]
+    public async Task<int> Sleep(Person p)
+    {
+        await Task.Delay(1000);
+        return p.Name.Length;
+    }
+
+    [LastStep]
+    public void LogMessage(Person x)
+    {
+        Console.WriteLine("Incoming Message: " + x?.Name);
+    }
+
+
+}
