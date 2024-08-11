2b50733db0bbd33bba5517a17062cf72b27df3bb
Andrei Ignat <ignatandrei@yahoo.com>
2024-08-11 14:38:19 +0300

    last blog

--- v2/Generator/MultiGeneratorV2.cs
+++ v2/Generator/MultiGeneratorV2.cs
@@ -715,7 +715,7 @@ public class MultiGeneratorV2
         var x = 0;
         x++;
         //if(x>2)
-        var lastGenerator = "ThisAssembly.Metadata";
+        var lastGenerator = "fluentify";
         var latest = generators[lastGenerator];
         await Task.WhenAll(_AllDescriptions
             .OrderByDescending(it => it.generatedDate)
