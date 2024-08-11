c97170e22a201e973b088ee79d5ef0efd48d5349
Andrei Ignat <ignatandrei@yahoo.com>
2024-08-11 20:21:03 +0300

    show relations

--- v2/Generator/FileWithContent.cs
+++ v2/Generator/FileWithContent.cs
@@ -2,4 +2,7 @@
 
 namespace Generator;
 
-public record FileWithContent(string fullPathFile, string file, string content);
+public record FileWithContent(string fullPathFile, string file, string content)
+{
+    public string extension => Path.GetExtension(fullPathFile);
+}
