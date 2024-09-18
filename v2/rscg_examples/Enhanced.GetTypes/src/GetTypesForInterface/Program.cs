43c1139cd7c470554174f0e1c58801fd5115348d
Andrei Ignat <ignatandrei@yahoo.com>
2024-09-18 19:44:31 +0300

    adding example

---
+++ v2/rscg_examples/Enhanced.GetTypes/src/GetTypesForInterface/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GetTypesForInterface;
+
+Console.WriteLine("Hello, World!");
+foreach (var type in ProjectTypes.GetIPersonTypes())
+{
+    Console.WriteLine(type.Name);
+}
