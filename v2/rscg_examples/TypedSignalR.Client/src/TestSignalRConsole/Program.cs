481696aabdcb6034c9f6d4b2971ddbe91857d84a
Andrei Ignat <ignatandrei@yahoo.com>
2024-10-27 01:11:14 +0300

    app host vers 1

---
+++ v2/rscg_examples/TypedSignalR.Client/src/TestSignalRConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.AspNetCore.SignalR.Client;
+
+Console.WriteLine("Hello, World!");
+await Task.Delay(5_000);
+HubConnection _connection = new HubConnectionBuilder()
+    .WithUrl("https://localhost:7302/ChatHub")
+    .Build();
+await _connection.StartAsync();
+
+_connection.On<string, string>("ReceiveMessage", (user, message) =>
+{
+    Console.WriteLine($"{user}: {message}");
+});
+while (true)
+{
+    await Task.Delay(1_000);
+    Console.WriteLine("what message do you want to send?");
+    var message = Console.ReadLine();
+    await _connection.InvokeAsync("SendMessage", "Console", message);
+}
---
+++ v2/rscg_examples/TypedSignalR.Client/src/TestSignalRHub.ApiService/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRSwaggerGen.Attributes;
+
+namespace TestSignalRHub.ApiService.Hubs;
+[SignalRHub]
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
