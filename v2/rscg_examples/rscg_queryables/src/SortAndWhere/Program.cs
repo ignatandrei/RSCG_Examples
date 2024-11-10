5f9dd4ebf3a810129f137269d66d8cc770e44cca
Andrei Ignat <ignatandrei@yahoo.com>
2024-11-10 19:38:53 +0200

    added data

---
+++ v2/rscg_examples/rscg_queryables/src/SortAndWhere/Program.cs
@@ -0,0 +1,26 @@
+﻿using SortAndWhere;
+
+
+Console.WriteLine("Hello, World!");
+
+var students = new Student[]
+{
+    new Student { FirstName = "John", LastName = "Doe", StartYear = 1935},
+    new Student { FirstName = "Ignat", LastName = "Andrei", StartYear = 1989 },
+};
+
+var orderedExplicitly = students.OrderBy(p => p.FirstName).ToArray();
+var orderedImplicitly = students.OrderBy("firStnaMe").ToArray();
+var orderedImplicitly2 = students.AsQueryable().OrderBy("fIrsTnAme").ToArray();
+
+
+//Search by property name
+
+var search = students.AsQueryable().Where("firstName", rscg_queryablesCommon.WhereOperator.Equal, "John").ToArray();
+
+Console.WriteLine("found : " + search.Length);
+
+search = students.AsQueryable().Where(Student_.Where_Expr("firstName", rscg_queryablesCommon.WhereOperator.Equal, "John")).ToArray();
+Console.WriteLine("found : " + search.Length);
+
+
---
+++ v2/rscg_examples/rscg_queryables/src/SortAndWhere/Student.cs
@@ -0,0 +1,13 @@
+﻿using rscg_queryablesCommon;
+
+namespace SortAndWhere;
+[MakeSortable]
+[MakeWhere]
+public class Student
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+
+    public int StartYear { get; set; }
+
+}
