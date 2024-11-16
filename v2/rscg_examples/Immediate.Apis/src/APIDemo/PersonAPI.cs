45e896c847f790bf0a2afabfbc18d640432a1f48
Andrei Ignat <ignatandrei@yahoo.com>
2024-11-16 21:05:38 +0200

    added first

---
+++ v2/rscg_examples/Immediate.Apis/src/APIDemo/PersonAPI.cs
@@ -0,0 +1,21 @@
+﻿using Immediate.Apis.Shared;
+using Immediate.Handlers.Shared;
+using Microsoft.AspNetCore.Http.HttpResults;
+namespace APIDemo;
+
+    [Handler]
+    [MapGet("/users")]
+    public static partial class PersonAPI
+    {
+        public record Query;
+
+        private static async ValueTask<Person[]> HandleAsync(
+            Query _,
+            CancellationToken token)
+        {
+            await Task.Delay(1000);
+            return new[] { new Person { FirstName = "Ignat", LastName = "Andrei" } };
+         }
+    }
+    
+    
