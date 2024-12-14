d0a29f57acab3870633104d772fe36f5c3d4939c
Andrei Ignat <ignatandrei@yahoo.com>
2024-12-14 16:15:55 +0200

    example

---
+++ v2/rscg_examples/Darp.BinaryObjects/src/BitsDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using BitsDemo;
+using Darp.BinaryObjects;
+
+var z = new zlib_header(0x78, 0x9C);
+
+var size = z.GetByteCount();
+
+// Write the values back to a buffer
+var writeBuffer = new byte[size];
+if(z.TryWriteLittleEndian(writeBuffer))
+{
+    Console.WriteLine("writing to buffer" );
+	foreach (var item in writeBuffer)
+	{
+		Console.Write(item+" ");
+	}
+}
