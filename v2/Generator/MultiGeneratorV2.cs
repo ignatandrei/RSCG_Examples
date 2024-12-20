e7558072a4c2a4ea99297f10b21e09053d600aed
Andrei Ignat <ignatandrei@yahoo.com>
2024-12-20 21:54:58 +0200

    binary

--- v2/Generator/MultiGeneratorV2.cs
+++ v2/Generator/MultiGeneratorV2.cs
@@ -719,7 +719,7 @@ public class MultiGeneratorV2
         //var x = 0;
         //x++;
         //if(x>2)
-        var lastGenerator = "Dolly";
+        var lastGenerator = "Darp.BinaryObjects";
         var latest = generators[lastGenerator];
         await Task.WhenAll(_AllDescriptions
             .OrderByDescending(it => it.generatedDate)
