958d807e5b96940360bca761605ebf23aba41bb8
Andrei Ignat <ignatandrei@yahoo.com>
2025-01-18 15:21:28 +0200

    demogit

---
+++ v2/rscg_examples/Larcanum.GitInfo/src/DemoGit/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"Version: {DemoGit.GitInfo.Branch}");
+Console.WriteLine($"Version: {DemoGit.GitInfo.CommitDate}");
