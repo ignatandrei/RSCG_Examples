d61cf7f7db960bf71c80768648af60d5de075e71
Andrei Ignat <ignatandrei@yahoo.com>
2025-01-21 15:35:37 +0200

    first version

---
+++ v2/rscg_examples/rscg_Interface_to_null_object/src/NullInterface/IDepartment.cs
@@ -0,0 +1,9 @@
+﻿namespace NullInterface;
+
+using InterfaceToNullObject;
+
+[ToNullObject]
+public interface IDepartment
+{
+    public string Name { get; set; }
+}
---
+++ v2/rscg_examples/rscg_Interface_to_null_object/src/NullInterface/Program.cs
@@ -0,0 +1,12 @@
+﻿using NullInterface;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World!");
+IDepartment department = new Department_null();
+department.Name = "IT";
+IEmployee employee = new Employee_null();
+employee.FirstName = "Andrei";
+employee.Department = department;
+Console.WriteLine(employee.FirstName);
+Console.WriteLine(employee.Department.Name);
