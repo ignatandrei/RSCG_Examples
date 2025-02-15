ce9685265de439045b84c160109cd5a302052ff4
Andrei Ignat <ignatandrei@yahoo.com>
2025-02-15 23:38:53 +0200

    first try

---
+++ v2/rscg_examples/DependencyModules.SourceGenerator/src/InjectDemo/Database.cs
@@ -0,0 +1,20 @@
+﻿using DependencyModules.Runtime.Attributes;
+
+namespace InjectDemo;
+[SingletonService]
+partial class Database : IDatabase
+{
+    private readonly IDatabase con;
+
+    public Database(IDatabase con)
+    {
+        this.con = con;
+    }
+    public void Open()
+    {
+        Console.WriteLine($"open from database");
+        con.Open();
+    }
+
+}
+
---
+++ v2/rscg_examples/DependencyModules.SourceGenerator/src/InjectDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using DependencyModules.Runtime;
+using InjectDemo;
+using Microsoft.Extensions.DependencyInjection;
+
+
+var serviceCollection = new ServiceCollection();
+
+//serviceCollection.AddModule<MyModule>();
+
+//var provider = serviceCollection.BuildServiceProvider();
+
+//var service = provider.GetService<Database>();
