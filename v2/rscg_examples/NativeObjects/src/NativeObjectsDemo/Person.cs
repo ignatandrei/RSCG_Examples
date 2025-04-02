d10cc8aa7f4179cc8d180cc523b6734b31512fa0
Andrei Ignat <ignatandrei@yahoo.com>
2025-04-02 21:23:29 +0300

    examples

---
+++ v2/rscg_examples/NativeObjects/src/NativeObjectsDemo/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace NativeObjectsDemo;
+[NativeObject]
+public interface IPerson
+{
+    public int CalculateAge();
+}
+class Person : IPerson
+{
+    public DateTime DateOfBirth { get; set; }
+
+    public int CalculateAge()
+    {
+
+        return (int)DateTime.Now.Subtract(DateOfBirth).TotalDays / 365;
+    }
+}
