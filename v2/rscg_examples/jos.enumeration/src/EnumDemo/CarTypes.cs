c95e4afc34ae52e4e7125e05df712e9d7254f80a
Andrei Ignat <ignatandrei@yahoo.com>
2025-08-15 12:12:14 +0300

    first iteration

---
+++ v2/rscg_examples/jos.enumeration/src/EnumDemo/CarTypes.cs
@@ -0,0 +1,17 @@
+﻿using JOS.Enumeration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDemo;
+
+
+partial  record CarTypes : IEnumeration<CarTypes>
+{
+    public static readonly CarTypes Dacia = new(1, "Dacia");
+    public static readonly CarTypes Tesla = new(2, "Tesla");
+    public static readonly CarTypes BMW = new(3, "BMW");
+    public static readonly CarTypes Mercedes = new(4, "Mercedes");
+}
