cae55258e6006be40d717d6ff53c49fc194fc4a1
Andrei Ignat <ignatandrei@yahoo.com>
2025-08-15 18:05:49 +0300

    first iteration

---
+++ v2/rscg_examples/Tortuga.Shipwright/src/MixinConsoleDemo/MixinConsoleDemo/Employee.cs
@@ -0,0 +1,8 @@
+﻿using Tortuga.Shipwright;
+
+namespace MixinConsoleDemo;
+[UseTrait(typeof(Person))]
+internal partial class Employee
+{
+    public decimal Salary { get; set; }
+}
---
+++ v2/rscg_examples/Tortuga.Shipwright/src/MixinConsoleDemo/MixinConsoleDemo/Person.cs
@@ -0,0 +1,11 @@
+﻿using Tortuga.Shipwright;
+
+namespace MixinConsoleDemo;
+internal class Person
+{
+    [Expose]
+    public string Name { get; set; } = string.Empty;
+    [Expose] 
+    public int Age { get; set; }
+    
+}
