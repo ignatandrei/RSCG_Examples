a87ac5fe2aff9ead251dedb383bea5c978420fea
Andrei Ignat <ignatandrei@yahoo.com>
2025-08-16 21:09:40 +0300

    last

--- v2/Generator/Program.cs
+++ v2/Generator/Program.cs
@@ -154,7 +154,7 @@ $$"""
 
     long nr = await m.GenerateMSFT();
     Console.WriteLine("RSCG used by MSFT :" + nr);
-    var lastGenerator = "Strings.ResourceGenerator";
+    var lastGenerator = "InlineComposition";
 
     await m.WrotePost(lastGenerator);     
     await m.WroteDocusaurusAll(lastGenerator);
