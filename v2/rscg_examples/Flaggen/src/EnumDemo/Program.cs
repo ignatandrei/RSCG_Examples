d6e839a5e1b354c4267b8eab51cb7c723e7a036e
Andrei Ignat <ignatandrei@yahoo.com>
2025-08-18 07:07:23 +0300

    first iteration

---
+++ v2/rscg_examples/Flaggen/src/EnumDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EnumDemo;
+
+Console.WriteLine("Hello, World!");
+var color = Colors.Red ;
+Console.WriteLine($"Selected Colors: {color}");
+color.Add(Colors.Blue);
+Console.WriteLine($"After Adding Blue: {color}");
+color.Remove(Colors.Red);
+Console.WriteLine($"After Removing Red: {color}");
+color.Toggle(Colors.Green);
