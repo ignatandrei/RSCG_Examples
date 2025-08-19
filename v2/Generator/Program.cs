7ed4d7205253da414c3ce5952ea8f55c1872fe37
Andrei Ignat <ignatandrei@yahoo.com>
2025-08-19 22:58:52 +0300

    AssemblyVersionInfo

--- v2/Generator/Program.cs
+++ v2/Generator/Program.cs
@@ -154,7 +154,7 @@ $$"""
 
     long nr = await m.GenerateMSFT();
     Console.WriteLine("RSCG used by MSFT :" + nr);
-    var lastGenerator = "InlineComposition";
+    var lastGenerator = "AssemblyVersionInfo";
 
     await m.WrotePost(lastGenerator);     
     await m.WroteDocusaurusAll(lastGenerator);
