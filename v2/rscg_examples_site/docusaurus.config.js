67f01f8404e76b4ee0d1b2ab2ce97ca9229b66e2
Andrei Ignat <ignatandrei@yahoo.com>
2025-08-27 22:59:05 +0300

    added zip

--- v2/rscg_examples_site/docusaurus.config.js
+++ v2/rscg_examples_site/docusaurus.config.js
@@ -51,8 +51,6 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
