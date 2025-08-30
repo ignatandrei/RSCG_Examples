093e086e8b7dbb6b7445384d411d19b4537c1fba
Andrei Ignat <ignatandrei@yahoo.com>
2025-08-30 16:16:39 +0300

    first

---
+++ v2/rscg_examples/Nino/src/Serializer/Person.cs
@@ -0,0 +1,14 @@
+﻿
+using Nino.Core;
+
+namespace SerializerDemo;
+[NinoType]
+public partial class Person
+{
+    public int Age;
+
+    public string Name = string.Empty;
+
+    
+}
+
---
+++ v2/rscg_examples/Nino/src/Serializer/Program.cs
@@ -0,0 +1,8 @@
+﻿using Nino.Core;
+using SerializerDemo;
+
+var p= new Person() { Name= "Andrei Ignat" , Age=55};
+var str= NinoSerializer.Serialize(p);
+//Console.WriteLine(str);
+var entity = NinoDeserializer.Deserialize<Person>(str);
+Console.WriteLine("name is "+entity.Name);
