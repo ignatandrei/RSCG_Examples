37fb4dd74378ded79171254ee029e191d6fc25fc
Andrei Ignat <ignatandrei@yahoo.com>
2025-08-31 16:40:23 +0300

    finch

---
+++ v2/rscg_examples/Finch.Generators/src/DB/Person.cs
@@ -0,0 +1,10 @@
+﻿using Finch.Abstractions;
+
+namespace DB;
+[GenerateSqlserverConnectionExtensions] 
+public partial class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+}
