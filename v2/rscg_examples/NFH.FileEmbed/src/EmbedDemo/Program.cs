8e228313f041ab2728967bbf7d36fa47917926de
Andrei Ignat <ignatandrei@yahoo.com>
2025-08-31 12:24:17 +0300

    start demo

---
+++ v2/rscg_examples/NFH.FileEmbed/src/EmbedDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmbedDemo;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var data= TestData.Countries();
+
+Console.WriteLine(Encoding.UTF8.GetString(data) );
