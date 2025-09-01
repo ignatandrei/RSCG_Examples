c2fb23cbb7a4d723b50714f5a8af83e3e031ef17
Andrei Ignat <ignatandrei@yahoo.com>
2025-09-01 07:38:45 +0300

    factory of factory

---
+++ v2/rscg_examples/FactoryGenerator.Abstractions/src/InjectDemo/Person.cs
@@ -0,0 +1,48 @@
+﻿
+using FactoryGenerator.Abstractions;
+
+namespace InjectDemo;
+public enum PersonType
+{
+    Employee,
+    Customer
+}
+[GenerateIFactory<PersonType>]
+public class PersonFactory
+{
+    private readonly PersonType person;
+
+    public PersonFactory(PersonType person)
+    {
+        this.person = person;
+    }
+    public Person Create()
+    {
+        return person switch
+        {
+            PersonType.Employee => new Employee()
+            {
+                TypeName = nameof(Employee)
+            },
+            PersonType.Customer => new Customer()
+            {
+                TypeName = nameof(Customer)
+            },
+            _ => throw new NotImplementedException()
+        };
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; } = string.Empty;
+    public string TypeName { get;init; } = string.Empty;
+}
+
+public class Employee : Person
+{
+}
+public class Customer : Person
+{
+}
+
