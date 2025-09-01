e57e5fee9c341fbca857b31bea634e7f973d7505
Andrei Ignat <ignatandrei@yahoo.com>
2025-09-01 20:57:32 +0300

    start schema

---
+++ v2/rscg_examples/Schema/src/Serializer/Person.cs
@@ -0,0 +1,18 @@
+﻿
+using schema.binary;
+using schema.binary.attributes;
+
+namespace SerializerDemo;
+
+[BinarySchema]
+[Endianness(Endianness.BigEndian)]
+public partial class Person : IBinaryConvertible
+{
+    [IntegerFormat(SchemaIntegerType.INT32)]
+    public int Age { get; set; }
+
+    [NullTerminatedString]
+    public string Name { get; set; } = string.Empty;
+
+}
+
---
+++ v2/rscg_examples/Schema/src/Serializer/Program.cs
@@ -0,0 +1,15 @@
+﻿using schema.binary;
+using SerializerDemo;
+
+var p= new Person() { Name= "Andrei Ignat" , Age=55};
+var ms=new MemoryStream();
+SchemaBinaryWriter writer = new ();
+p.Write(writer);
+await writer.CompleteAndCopyToAsync(ms);
+ms.Position=0;
+SchemaBinaryReader reader = new (ms);
+p.Name=string.Empty;
+p.Age=0;
+p.Read(reader);
+Console.WriteLine(p.Name);
+Console.WriteLine(p.Age);
