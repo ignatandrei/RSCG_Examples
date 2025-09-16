66bc296e5c06c82a4d1b4499b1a4ab36b2584226
Andrei Ignat <ignatandrei@yahoo.com>
2025-09-16 19:03:36 +0300

    facet1

---
+++ v2/rscg_examples/Facet/src/mapperDemo/PersonDTO.cs
@@ -0,0 +1,13 @@
+﻿
+using Facet;
+
+namespace mapperDemo;
+[Facet(typeof(Person), Kind = FacetKind.Struct)]
+public partial struct PersonDTO
+{   public string FullName { 
+        get
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
---
+++ v2/rscg_examples/Facet/src/mapperDemo/Program.cs
@@ -0,0 +1,6 @@
+﻿using mapperDemo;
+var p=new Person();
+p.FirstName = "Andrei";
+p.LastName = "Ignat";
+PersonDTO dto= new(p);
+Console.WriteLine(dto.FullName);
