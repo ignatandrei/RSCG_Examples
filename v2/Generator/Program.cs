e5c31b7e193568316650d2587800d52b1d25cb5c
Andrei Ignat <ignatandrei@yahoo.com>
2025-10-01 21:51:29 +0300

    kli.Localize

--- v2/Generator/Program.cs
+++ v2/Generator/Program.cs
@@ -154,7 +154,7 @@ $$"""
 
     long nr = await m.GenerateMSFT();
     Console.WriteLine("RSCG used by MSFT :" + nr);
-    var lastGenerator = "mvvmgen";
+    var lastGenerator = "kli.Localize";
 
     await m.WrotePost(lastGenerator);     
     await m.WroteDocusaurusAll(lastGenerator);
