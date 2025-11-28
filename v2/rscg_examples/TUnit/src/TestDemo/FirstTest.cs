99ae9900fc51f68059d8291a0353a21b14726be4
Andrei Ignat <ignatandrei@yahoo.com>
2025-11-28 19:41:24 +0200

    added tunit

---
+++ v2/rscg_examples/TUnit/src/TestDemo/FirstTest.cs
@@ -0,0 +1,13 @@
+﻿namespace TestDemo;
+
+
+public class FirstTest
+{
+    [Test]
+    public async Task Add_WithTwoNumbers_ReturnsSum()
+    {
+        var result = true;
+        // Assert
+        await Assert.That(result).IsTrue();
+    }
+}
