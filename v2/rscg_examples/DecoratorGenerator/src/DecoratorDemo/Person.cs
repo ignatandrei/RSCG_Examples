d14a8c23ccc3f43e5f7d2a8ceb59e5d64a7b4e17
Andrei Ignat <ignatandrei@yahoo.com>
2025-11-29 08:30:06 +0200

    Add DecoratorGenerator example with demo project
    Introduced a new example for the DecoratorGenerator source generator, including metadata in GeneratorDataRec.json and a complete demo project. The demo demonstrates decorating an IPerson interface, logging property access and method calls using a LogPerson decorator.

---
+++ v2/rscg_examples/DecoratorGenerator/src/DecoratorDemo/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorDemo;
+
+internal class Person : IPerson
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string FullName()
+    {
+        return $"{FirstName} {LastName}";
+    }
+    public async Task<int> CalculateAgeAsync(DateTime birthDate)
+    {
+        await Task.Delay(100); // Simulate async work
+        var today = DateTime.Today;
+        var age = today.Year - birthDate.Year;
+        if (birthDate.Date > today.AddYears(-age)) age--;
+        return age;
+    }
+
+}
---
+++ v2/rscg_examples/DecoratorGenerator/src/DecoratorDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DecoratorDemo;
+
+Console.WriteLine("Hello, World!");
+IPerson person = new Person();
+person = new LogPerson(person);
+person.FirstName = "Andrei";
+person.LastName = "Ignat";
+Console.WriteLine(person.FullName());
+var birthDate = new DateTime(1970, 4, 16);
+var age = await person.CalculateAgeAsync(birthDate);
+Console.WriteLine($"Age is {age}");
