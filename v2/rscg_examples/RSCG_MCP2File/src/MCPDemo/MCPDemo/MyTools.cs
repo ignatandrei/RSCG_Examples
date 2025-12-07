b60f2995d5925bb565817b942ff36ce6588ac0b2
Andrei Ignat <ignatandrei@yahoo.com>
2025-12-07 10:52:37 +0200

    start demo

---
+++ v2/rscg_examples/RSCG_MCP2File/src/MCPDemo/MCPDemo/MyTools.cs
@@ -0,0 +1,19 @@
+﻿using ModelContextProtocol.Server;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MCPDemo;
+[MCP2File.AddMCPExportToFile()]
+partial class MyTools
+{
+    [McpServerTool]
+    [Description("Echo demo")]
+    public async Task<string> SendEcho([Description("echo")] string echoData)
+    {
+        await Task.Delay(10);
+        return echoData;
+    }
+
+}
