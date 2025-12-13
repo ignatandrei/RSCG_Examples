b279f38b51d6b010e04fdb07a7af97acd9fb3a84
Andrei Ignat <ignatandrei@yahoo.com>
2025-12-13 20:17:11 +0200

    primitive

---
+++ v2/rscg_examples/Monify/src/PrimitiveDemo/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimitiveDemo;
+
+[Monify.Monify<int>]
+public partial struct Age;
+internal class Person
+{
+    public Age Age { get; set; }
+    public bool IsAdult => Age >= 18;
+}
---
+++ v2/rscg_examples/Monify/src/PrimitiveDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PrimitiveDemo;
+
+Console.WriteLine("Hello, World!");
+Person p= new ();
+p.Age = 55;
+Console.WriteLine($"Person's age is {p.Age}");
+Console.WriteLine($"Is adult: {p.IsAdult}");
