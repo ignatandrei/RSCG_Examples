acd86ab805488a644986dc62caf258d17c4d3bf5
Andrei Ignat <ignatandrei@yahoo.com>
2025-12-20 08:52:29 +0200

    start

---
+++ v2/rscg_examples/Silhouette/src/ProfilerDemo/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://andrewlock.net/creating-a-dotnet-profiler-using-csharp-with-silhouette/
+Console.WriteLine("Please read https://andrewlock.net/creating-a-dotnet-profiler-using-csharp-with-silhouette/");
