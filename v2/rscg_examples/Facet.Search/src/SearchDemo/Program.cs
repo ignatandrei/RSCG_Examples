0e22ba74870dcee530d11dcf2df019485fbd48bf
Andrei Ignat <ignatandrei@yahoo.com>
2025-12-20 10:34:09 +0200

    frist

---
+++ v2/rscg_examples/Facet.Search/src/SearchDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SearchDemo;
+using SearchDemo.Search;
+
+var filter = new PersonSearchFilter
+{
+    DOBFrom = new DateTime(1970, 1, 1),
+    DOBTo = new DateTime(1980, 12, 31),
+    IsActive = true,
+    MinSalary=1,
+    MaxSalary= 10,
+    SearchText= "Andrei"
+
+
+};
+
+MyAppContext cnt = new ();
+var p= cnt.Person.ApplyFacetedSearch(filter);
+var sql = p.ToQueryString();
+Console.WriteLine(sql);
