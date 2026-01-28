5384f5ca99359c5c818af3fdb20231d8f40ee1f4
Andrei Ignat <ignatandrei@yahoo.com>
2026-01-28 18:10:19 +0200

    first version

---
+++ v2/rscg_examples/RSCG_idempotency/src/IdempotencyDemo/Purchase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdempotencyDemo;
+
+internal partial class Purchase
+{
+    public bool PurchaseNow([RSCG_IdemPotency.Idempotent]string UniqueId, string idProduct,int quantity)
+    {
+        if(PurchaseNow_ExistsBefore(UniqueId))
+            return false;
+
+        Console.WriteLine($"bought ! ");
+        return true;
+    }
+}
--- v2/rscg_examples_site/docs/Authors/AndreiIgnat.md
+++
@@ -1,7 +0,0 @@
-# Author : AndreiIgnat
-
-Number RSCG: 1
-
-
-        1 [RSCG_ExportDiagram](/docs/RSCG_ExportDiagram) [![Nuget](https://img.shields.io/nuget/dt/RSCG_ExportDiagram?label=RSCG_ExportDiagram)](https://github.com/ignatandrei/RSCG_ExportDiagram) ![GitHub Repo stars](https://img.shields.io/github/stars/ignatandrei/RSCG_ExportDiagram?style=social) 2024-08-01
-    
