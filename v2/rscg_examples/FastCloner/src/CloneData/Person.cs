afb6ae3283f361959195067c7445e3ec35f9d0ec
Andrei Ignat <ignatandrei@yahoo.com>
2026-02-03 07:30:19 +0200

    example

---
+++ v2/rscg_examples/FastCloner/src/CloneData/Person.cs
@@ -0,0 +1,14 @@
+﻿
+using FastCloner.SourceGenerator.Shared;
+
+namespace CloneData;
+[FastClonerClonable]
+public partial class Person
+{
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public int Age { get; set; }
+    public string Name() => $"{FirstName} {LastName}";
+
+    public Person[] Childs { get; set; } = [];
+}
