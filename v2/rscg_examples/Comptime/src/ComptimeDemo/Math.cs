cf3d96c10f6d423ce34206ea9ac71a8ccd5a7655
Andrei Ignat <ignatandrei@yahoo.com>
2026-02-06 06:55:21 +0200

    try first

---
+++ v2/rscg_examples/Comptime/src/ComptimeDemo/Math.cs
@@ -0,0 +1,24 @@
+﻿using Comptime;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComptimeDemo;
+
+public static partial class Math
+{
+    [Comptime]
+    public static int SumList(IReadOnlyList<int> numbers)
+    {
+        return numbers.Sum();
+    }
+    [Comptime]
+    public static long Factorial(int n)
+    {
+        if (n <= 1) return 1;
+        long result = 1;
+        for (int i = 2; i <= n; i++)
+            result *= i;
+        return result;
+    }
+}
---
+++ v2/rscg_examples/Comptime/src/ComptimeDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+var sum2 = ComptimeDemo.Math.SumList(new[] { 10, 20, 30 });
+Console.WriteLine($"Sum: {sum2}");
+var x=ComptimeDemo.Math.Factorial(5);
+Console.WriteLine($"Factorial: {x}");
+
