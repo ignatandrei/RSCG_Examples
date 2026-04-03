45bc7855a0817fc62140c2e3aedd11b199194b68
Andrei Ignat <ignatandrei@yahoo.com>
2026-04-03 10:37:21 +0300

    first iter

---
+++ v2/rscg_examples/NLog.Extensions.ThisClass/src/LogDemo/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogDemo;
+
+[ClassLoggerLazy]
+partial class Person
+{
+    public string FirstName { get; set; }= string.Empty;
+    public string LastName { get; set; } = string.Empty;
+
+    public string Name()
+    {
+        Logger.Error("This is an error message from the Name method.");
+        return $"{FirstName} {LastName}";
+    }
+
+}
---
+++ v2/rscg_examples/NLog.Extensions.ThisClass/src/LogDemo/Program.cs
@@ -0,0 +1,5 @@
+﻿using LogDemo;
+
+Console.WriteLine("Hello, World!");
+var person = new Person() { FirstName = "Andrei", LastName = "Ignat" };
+Console.WriteLine(person.Name());
