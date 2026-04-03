566bd2099e7ca97995f1ad61d7b7b1fb4343d231
Andrei Ignat <ignatandrei@yahoo.com>
2026-04-03 15:14:38 +0300

    first v

---
+++ v2/rscg_examples/TaggedEnum/src/EnumDemo/CarTypes.cs
@@ -0,0 +1,22 @@
+﻿
+using TaggedEnum;
+
+namespace EnumDemo;
+
+[Tagged]
+public enum CarTypes 
+{
+    [Data("this is none")]
+    None,
+    [Data("this is dacia")]
+    Dacia,
+    [Data("this is tesla")]
+
+    Tesla,
+    [Data("this is bwm")]
+
+    BMW,
+    [Data("this is mercedes")]
+
+    Mercedes,
+}
---
+++ v2/rscg_examples/TaggedEnum/src/EnumDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EnumDemo;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(CarTypes.None.ToStringFast());
+Console.WriteLine(CarTypes.None.Data);
+Console.WriteLine(EnumDemoCarTypesExtension.TryGetValueByName("None",out _));
+//Console.WriteLine("Car types:" + CarTypesEnumExtensions.GetValues().Count);
+//var cars = CarTypesEnumExtensions.GetValues();
+//foreach (var car in cars)
+//{
+//    Console.WriteLine(car.ToStringFast());
+//}
+
