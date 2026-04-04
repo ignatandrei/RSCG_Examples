322bb81356d44dd571c10b4166fdb8677f886833
Andrei Ignat <ignatandrei@yahoo.com>
2026-04-04 09:26:36 +0300

    type providers

---
+++ v2/rscg_examples/Maestria.TypeProviders/src/DemoExcel/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoExcel;
+
+[ExcelProvider(TemplatePath = @"Excel.xlsx")]
+public partial class MyExcelPerson
+{
+}
