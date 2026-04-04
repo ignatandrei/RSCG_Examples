e4bd73949de64c7c9cccf56c64d332b88278bd8d
Andrei Ignat <ignatandrei@yahoo.com>
2026-04-04 11:26:41 +0300

    Update index.js

--- v2/rscg_examples_site/src/pages/index.js
+++ v2/rscg_examples_site/src/pages/index.js
@@ -20,10 +20,7 @@ function HomepageHeader() {
             to="/docs/List-of-RSCG">
             See examples 
           </Link>
-          <div>
-            Search for a component:
-            <input type="text" placeholder="Search..." />
-          </div>
+          
         </div>
       </div>
     </header>
