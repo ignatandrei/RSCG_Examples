4e2fa1e253c427e2c0980d25ced9a2dcd4e28e32
Andrei Ignat <ignatandrei@yahoo.com>
2026-04-04 13:52:50 +0300

    demo

---
+++ v2/rscg_examples/Aigamo.MatchGenerator/src/EnumDemo/CarTypes.cs
@@ -0,0 +1,24 @@
+﻿using Aigamo.MatchGenerator;
+namespace EnumDemo;
+
+[GenerateMatch]
+public enum CarTypes 
+{
+    None,
+    Dacia,
+    
+    Tesla,
+    
+    BMW,
+    
+    Mercedes,
+}
+
+
+[GenerateMatch]
+abstract record MaritalStatus;
+
+sealed record Single : MaritalStatus;
+sealed record Married : MaritalStatus;
+sealed record Divorced : MaritalStatus;
+sealed record Widowed : MaritalStatus;
