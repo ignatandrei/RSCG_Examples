cceb31ad21201ba8d021add76840a7045624830b
Andrei Ignat <ignatandrei@yahoo.com>
2026-08-18 21:39:08 +0300

    first

---
+++ v2/rscg_examples/Atulin.AutoDbSet/src/EntityDemo/DotNetStatsContext.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+namespace Stats.Database;
+
+[AutoDbSetGenerators.AutoDbContext]
+public partial class DotNetStatsContext : DbContext
+{
+    [SetsRequiredMembers]
+    internal DotNetStatsContext() : base() {
+        Stars = Set<Star>();
+        Projects = Set<Project>();
+    }
+    [SetsRequiredMembers]
+    public DotNetStatsContext(DbContextOptions<DotNetStatsContext> options)
+        : base(options)
+    {
+        Stars = Set<Star>();
+        Projects = Set<Project>();
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Project>(entity =>
+        {
+            //entity.ToTable("Project");
+
+            entity.Property(e => e.Id);//.HasColumnName("ID");
+            entity.Property(e => e.Description).HasMaxLength(500);
+            entity.Property(e => e.Name).HasMaxLength(50);
+            entity.Property(e => e.SourceCodeUrl).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Star>(entity =>
+        {
+        //    entity.Property(e => e.Id)
+        //        .HasColumnName("ID");
+        //    entity.Property(e => e.Idproject).HasColumnName("IDProject");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ v2/rscg_examples/Atulin.AutoDbSet/src/EntityDemo/Star.cs
@@ -0,0 +1,13 @@
+﻿namespace Stats.Database;
+
+[AutoDbSetGenerators.AutoDbSet]
+public partial class Star
+{
+    public long Id { get; set; }
+
+    public long Idproject { get; set; }
+
+    public int Count { get; set; }
+
+    public DateOnly DateRecording { get; set; }
+}
