7514f486d2d52f48dcc57f73841b76e8bcdbace5
Andrei Ignat <ignatandrei@yahoo.com>
2026-08-21 21:24:18 +0300

    Update Program.cs

--- v2/Generator/Program.cs
+++ v2/Generator/Program.cs
@@ -154,7 +154,7 @@ $$"""
 
     long nr = await m.GenerateMSFT();
     Console.WriteLine("RSCG used by MSFT :" + nr);
-    var lastGenerator = "serde";
+    var lastGenerator = "IncrementalSourceGenerator.BuilderPattern";
 
     await m.WrotePost(lastGenerator);     
     await m.WroteDocusaurusAll(lastGenerator);
