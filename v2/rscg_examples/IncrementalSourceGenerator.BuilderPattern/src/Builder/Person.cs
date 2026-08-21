47895d905b40979d0532b1116ab79fe6aa140097
Andrei Ignat <ignatandrei@yahoo.com>
2026-08-21 20:53:10 +0300

    example

---
+++ v2/rscg_examples/IncrementalSourceGenerator.BuilderPattern/src/Builder/Person.cs
@@ -0,0 +1,15 @@
+﻿using Generator;
+namespace Builder;
+
+[Builder(typeof(Person))]
+public partial class PersonBuilder
+{
+    
+}
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FullName() => $"{FirstName} {LastName}";
+}
