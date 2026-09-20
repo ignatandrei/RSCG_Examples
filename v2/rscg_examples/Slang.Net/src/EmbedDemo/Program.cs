b050974362fdfd98cf478693b1c0891cd14566cb
Andrei Ignat <ignatandrei@yahoo.com>
2026-09-20 16:17:01 +0300

    Slang.Net

---
+++ v2/rscg_examples/Slang.Net/src/EmbedDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿using EmbedDemo;
+using System.Globalization;
+
+Console.WriteLine("Hello, World!");
+
+TranslateAllText.SetCulture(new CultureInfo("en-US"));
+Console.WriteLine(TranslateAllText.Instance.Root.Page1.MyText);
+
+TranslateAllText.SetCulture(new CultureInfo("ro-RO")    );
+Console.WriteLine(TranslateAllText.Instance.Root.Page1.MyText);
---
+++ v2/rscg_examples/Slang.Net/src/EmbedDemo/Trans.cs
@@ -0,0 +1,6 @@
+﻿using Slang;
+
+namespace EmbedDemo;
+
+[Translations(InputFileName = "AllText")]
+public partial class TranslateAllText;
