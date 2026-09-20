a17e27b3a026a72864e0ea90d812cfc129aa1a3b
Andrei Ignat <ignatandrei@yahoo.com>
2026-09-20 20:17:18 +0300

    forgeest

--- v2/Generator/Program.cs
+++ v2/Generator/Program.cs
@@ -154,7 +154,7 @@ $$"""
 
     long nr = await m.GenerateMSFT();
     Console.WriteLine("RSCG used by MSFT :" + nr);
-    var lastGenerator = "LayeredCraft.OptimizedEnums";
+    var lastGenerator = "Forged";
 
     await m.WrotePost(lastGenerator);     
     await m.WroteDocusaurusAll(lastGenerator);
