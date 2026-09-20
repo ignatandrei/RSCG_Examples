053f70be61f26c8e0ad81f9d8493ff4cefd2b452
Andrei Ignat <ignatandrei@yahoo.com>
2026-09-20 17:09:13 +0300

    first try

---
+++ v2/rscg_examples/ZodSharp/src/Valid/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ZodSharp.SourceGenerators;
+
+namespace Valid;
+
+[ZodSchema]
+public partial class Person
+{
+    [Range(18, 199)]
+    public int Age { get; set; }
+
+    [Required]
+    [StringLength(50, MinimumLength = 3)]
+    public string Name { get; set; } = string.Empty;
+
+    
+}
---
+++ v2/rscg_examples/ZodSharp/src/Valid/Program.cs
@@ -0,0 +1,6 @@
+﻿using Valid;
+
+Console.WriteLine("Hello, World!");
+Person p= new (){ Age = 55, Name = "Andrei" };
+
+Console.WriteLine("Person is valid");
